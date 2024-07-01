e556be1c38ea6ecca23e9b6d85a1fa59e3a0f774
Bidhu1024 <bidhu1024av@gmail.com>
2024-07-01 21:14:51 +0530

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import "./App.css";
-
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import SignIn from "./Pages/SignIn/SignIn";
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignIn/>} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/Pages/SignIn/SignIn.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+import "./SignIn.css";
+const SignIn = () => {
+  return (
+    <Box className="main-div">
+      <Box className="card-main">
+        
+      </Box>
+    </Box>
+  );
+};
+
+export default SignIn;
